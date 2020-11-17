ef7e34eedb04007a3469d96809b894afec321053
unknown <kandu.jiwandono@gmail.com>
2020-11-17 22:30:15 +0700

    read data to R

--- house-pricing-analysis.R
+++ house-pricing-analysis.R
@@ -1,9 +1,5 @@
 library(tidyverse)
 
 
-train <- read_csv('D:/R Data/house-prices-advanced-regression-techniques/house-pricing-analysis/data/train.csv')
-train2 <- read_csv('D:/R Data/house-prices-advanced-regression-techniques/house-pricing-analysis/data/train.csv')
-
-
+df_train <- read_csv('data/train.csv')
 df_test <- read_csv("data/test.csv")
-df_test2 <- read_csv("data/test.csv")
