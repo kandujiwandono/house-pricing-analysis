ffb5d22b6988697a414ae35258efde021cfb1175
bulu <angga053@yahoo.com>
2020-11-17 22:14:18 +0700

    add train2

--- house-pricing-analysis.R
+++ house-pricing-analysis.R
@@ -1,3 +1,4 @@
 library(tidyverse)
 
 train <- read_csv('D:/R Data/house-prices-advanced-regression-techniques/house-pricing-analysis/data/train.csv')
+train2 <- read_csv('D:/R Data/house-prices-advanced-regression-techniques/house-pricing-analysis/data/train.csv')
